ce150383b86d93e090d9ce062f4580929b960178
MaaviaAsghar4 <maavia.asghar.96@gmail.com>
2020-09-02 23:20:52 +0500

    Changes in the app.js and app.css file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello World from Maavia Asghar</h1>
+     <h1 className="text-align">Hello World from Maavia Asghar</h1>
     </div>
   );
 }
